4653b03b6fdf71ebd9cf567d3cf8499317b6202b
Rafael souza <somethinho.dev@gmail.com>
2024-03-15 20:10:36 -0300

    Delete method added

--- src/controller/rules.js
+++ src/controller/rules.js
@@ -116,5 +116,33 @@ module.exports = {
          console.error(error)
          results.error = 'server internal error at put method.'
          return res.status(500).json(results.results)
-    }}
+    }},
+
+
+
+
+    async exclude(req, res) {
+    
+    const {id} = req.params;
+    
+    try {
+        
+        const checkId = await registrations.findOne({where: {id: id}})
+        if(!checkId){
+
+            error = 'Id user not found.'
+            return res.status(404).json(error)
+        }
+        else{
+        let excludeId = await registrations.destroy({where: {id: id}})
+        return res.status(200).json(
+        {message: `${excludeId} user excluded.`})
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Server internal error')
+    }
+
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ const controller = require('./controller/rules')
 
 router.get('/registration', controller.GetAll)
 router.get('/registration/:id', controller.GetOne)
+
 router.post('/registration', controller.create)
 router.put('/registration/:id', controller.update)
+router.delete('/registration/:id', controller.exclude)
 
 module.exports = router
